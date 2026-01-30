9c6cc898fa582366c6258d4208c27f1e487f7e18
hanan <hanan.sachs@mail.huji.ac.il>
2026-01-30 19:17:31 +0200

    added enemy class

--- src/entities.py
+++ src/entities.py
@@ -95,3 +95,56 @@ class Player(Character):
         """Remove the current mask and change sprite."""
         self.current_mask = None
         self._update_sprite_display()
+
+
+class Enemy(Character):
+    def __init__(self, x, y, sprite_img, lives=3, sprite_variants=None):
+        super().__init__(x, y, sprite_img, lives)
+        self.current_mask = None  # Holds the color of the current mask
+        self.speed = 4
+        self.facing_right = True  # Track sprite direction
+        self.base_image = sprite_img.copy()  # Store original sprite
+        self.sprite_variants = sprite_variants or {}  # Dict of mask color -> sprite image
+
+    def set_speed(self,player):
+        self.velocity=(5/((self.pos - player.pos)**2))*(self.pos - player.pos)
+        if (self.pos-player.pos)**2 <150:
+            self.velocity=self.velocity*5
+        # Update sprite direction based on movement
+        if self.velocity.x > 0:  # Moving right
+            if not self.facing_right:
+                self.facing_right = True
+                self._update_sprite_display()
+        elif self.velocity.x < 0:  # Moving left
+            if self.facing_right:
+                self.facing_right = False
+                self._update_sprite_display()
+
+    def _update_sprite_display(self):
+        """Update the displayed sprite based on current mask and facing direction."""
+        # Get the current sprite (base or masked variant)
+        current_sprite = self.base_image
+        if self.current_mask and self.current_mask in self.sprite_variants:
+            current_sprite = self.sprite_variants[self.current_mask]
+
+        # Apply facing direction
+        if not self.facing_right:
+            self.image = pygame.transform.flip(current_sprite, True, False)
+        else:
+            self.image = current_sprite.copy()
+
+    def update(self):
+        """Update player position."""
+        self.pos += self.velocity
+        self.rect.topleft = self.pos
+        self.set_speed()
+
+    def equip_mask(self, color):
+        """Equip a mask and change sprite."""
+        self.current_mask = color
+        self._update_sprite_display()
+
+    def unequip_mask(self):
+        """Remove the current mask and change sprite."""
+        self.current_mask = None
+        self._update_sprite_display()
